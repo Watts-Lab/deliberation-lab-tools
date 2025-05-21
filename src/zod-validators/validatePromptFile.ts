0871fbdb28987d533631e6f70f5f897150eb85c2
darrenmo12309 <darrenmo@sas.upenn.edu>
2025-05-21 10:31:06 -0400

    fixed typescript type errors for function arguments

--- src/zod-validators/validatePromptFile.ts
+++ src/zod-validators/validatePromptFile.ts
@@ -2,30 +2,21 @@ import { z, ZodIssue } from "zod";
 
 const seperatorSchema = z.string().regex(/^-{3,}$/);
 
-export const metadataSchema = (fileName) =>
-    z.object({
+export const metadataBaseSchema = z.object({
         name: z.string(),
         type: z.enum(["openResponse", "multipleChoice", "noResponse", "listSorter"]),
-    }).superRefine((data, ctx) => {
+        notes: z.string().optional(),
+    });
+
+export const metadataSchema = (fileName: string) =>
+    metadataBaseSchema.superRefine((data, ctx) => {
         if (data.name !== fileName) {
             ctx.addIssue({
                 code: z.ZodIssueCode.custom,
                 message: `name must match file path starting from repository root`,
-
+                path : ["name"],
             });
         }
     });
 
-
-//filename is address of string
-const nameSchema = (fileName) => z.string().superRefine((val, ctx) => {
-    const expected = `name: ${fileName}`;
-    if (val !== expected) {
-        ctx.addIssue({
-            code: z.ZodIssueCode.custom,
-            message: `Line identifying name must be of form "name: ${fileName}"`,
-        });
-    }
-});
-
-const typeSchema = z.enum(["openResponse", "multipleChoice", "noResponse", "listSorter"]);
+export type MetadataType = z.infer<typeof metadataBaseSchema>;
