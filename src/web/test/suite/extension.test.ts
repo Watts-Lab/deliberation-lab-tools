c6a95f59a2db63355bf5a4c3af12cbd0d3cab47a
trinif <trinilaurenf@gmail.com>
2025-05-27 12:23:07 -0400

    path in tests

--- src/web/test/suite/extension.test.ts
+++ src/web/test/suite/extension.test.ts
@@ -1,6 +1,6 @@
 import * as assert from 'assert';
 import { detectPromptMarkdown, detectTreatmentsYaml } from '../../extension';
-
+import * as path from 'path';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
