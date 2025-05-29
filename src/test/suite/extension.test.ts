80b8501e8a55936c4da33521dc9c2d58711cc3e0
trinif <trinilaurenf@gmail.com>
2025-05-29 16:04:45 -0400

    removing comments

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -4,8 +4,6 @@ import * as assert from 'assert';
 import { detectPromptMarkdown, detectTreatmentsYaml } from '../../extension';
 import { suite, test } from 'mocha';
 
-// CHANGE PACKAGE.JSON
-
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import { diagnosticCollection } from '../../extension';
