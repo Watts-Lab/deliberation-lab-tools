f5b85185ff074d82b744f3fe33d9390f77c44354
darrenmo12309 <darrenmo@sas.upenn.edu>
2025-06-17 17:59:31 -0400

    added indentation warnings

--- src/parsers/parseYaml.ts
+++ src/parsers/parseYaml.ts
@@ -6,7 +6,9 @@ import {
   treatmentFileSchema,
   TreatmentFileType,
 } from "../zod-validators/validateTreatmentFile";
-import { handleError } from "../errorPosition";
+import { handleError, offsetToPosition } from "../errorPosition";
+import { parse } from 'path';
+import { off } from 'process';
 
 // YAML validator for treatments
 
@@ -17,34 +19,52 @@ export function parseYaml(document: vscode.TextDocument) {
     const diagnostics: vscode.Diagnostic[] = [];
 
     // Parse YAML content into AST
-    let parsedData;
+    let parsedData = YAML.parseDocument(document.getText(), {
+        keepCstNodes: true,
+        keepNodeTypes: true,
+    } as any) || null;
+    console.log("YAML parsed successfully.");
+    console.log(parsedData.errors);
 
-    try {
-        parsedData = YAML.parseDocument(document.getText(), {
-            keepCstNodes: true,
-            keepNodeTypes: true,
-        } as any) || null;
-        console.log("YAML parsed successfully.");
-    } catch (error) {
-        if (error instanceof Error) {
+    if (parsedData.errors) {
+        console.log("YAML parsing errors found:", parsedData.errors);
+        parsedData.errors.forEach((error: any) => {
+            const position = offsetToPosition(error.pos?.[0], document);
             const range = new vscode.Range(
-                new vscode.Position(0, 0),
-                new vscode.Position(0, 1)
+                position,
+                new vscode.Position(position.line, position.character + 10)
+            );
+            if (error.code === 'BAD_INDENT' || error.code === 'MISSING_CHAR' || 
+                error.code === 'BLOCK_AS_IMPLICIT_KEY' || error.code === 'MULTILINE_IMPLICIT_KEY') {
+                diagnostics.push(
+                    new vscode.Diagnostic(
+                        range,
+                        `YAML syntax error: ${error.code} -> ${error.message}; Check for proper indentation and formatting.`,
+                        vscode.DiagnosticSeverity.Error
+                    )
+                );
+            }
+        });
+        diagnosticCollection.set(document.uri, diagnostics);
+        console.log("Length of diagnostics for yaml: " + diagnostics.length);
+        console.log("Length of diagnostic collection for yaml: " + diagnosticCollection.get(document.uri)!!.length);
+        return;
+    } else if (parsedData.warnings) {
+        console.log("YAML parsing warnings found:", parsedData.warnings);
+        parsedData.warnings.forEach((warning: any) => {
+            const range = new vscode.Range(
+                offsetToPosition(warning.pos?.[0], document),
+                offsetToPosition(warning.pos?.[1], document)
             );
             diagnostics.push(
                 new vscode.Diagnostic(
                     range,
-                    `YAML syntax error: ${error.message}`,
-                    vscode.DiagnosticSeverity.Error
+                    `YAML syntax warning: ${warning.code} -> ${warning.message}`,
+                    vscode.DiagnosticSeverity.Warning
                 )
             );
-            diagnosticCollection.set(document.uri, diagnostics);
-            console.log("Length of diagnostics for yaml: " + diagnostics.length);
-            console.log("Length of diagnostic collection for yaml: " + diagnosticCollection.get(document.uri)!!.length);
-        }
-        parsedData = null;
-        return;
-    }
+        });
+    }   
 
     // Check if the YAML document is empty
     if (
